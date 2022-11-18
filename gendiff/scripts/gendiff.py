b73f28cb51463f5f8655149580e00606cd90f083
Andrey Ivanov <veny.xd@gmail.com>
2022-11-18 14:06:59 +0100

    gendiff init

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+args = parser.parse_args()
+
+
+def main():
+    print(args)
+
+
+if __name__ == ('__main__'):
+    main()
