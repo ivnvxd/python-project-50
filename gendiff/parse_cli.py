34c1f36dd4c2363562183b3b541a0e047804fb81
Andrey Ivanov <veny.xd@gmail.com>
2022-11-23 10:58:13 +0100

    fix docstring

--- gendiff/parse_cli.py
+++ gendiff/parse_cli.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def parse_arguments():
+def parse_arguments() -> argparse.Namespace:
     """
     Parses the data entered into the console to run the program.
 
@@ -9,7 +9,7 @@ def parse_arguments():
         - first_file (str): First file for comparison.
         - second_file (str): Second file for comparison.
         - format (str): Format for comparison (default: stylish).
-    :return: Parsed input values (argparse arguments)
+    :return: Parsed input values (argparse.Namespace)
     """
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
@@ -18,7 +18,9 @@ def parse_arguments():
     parser.add_argument("second_file")
 
     parser.add_argument(
-        '-f', '--format', help='set format of output'
+        '-f', '--format',
+        help='set format of output (default: \'stylish\')',
+        default='stylish'
     )
 
     args = parser.parse_args()
