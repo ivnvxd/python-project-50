3700fad11aa016e077ea6397041f46a604844f07
Andrey Ivanov <veny.xd@gmail.com>
2022-11-23 18:20:56 +0100

    refactor test files, add plain format tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,59 +4,59 @@ from gendiff.diff import generate_diff
 
 
 @pytest.fixture
-def json_plain():
-    link = {}
-    link['before'] = './tests/fixtures/file1_plain.json'
-    link['after'] = './tests/fixtures/file2_plain.json'
-    link['result'] = './tests/fixtures/result_plain.txt'
-    return link
+def files():
+    links = {
+        'file1_json_flat': './tests/fixtures/json/file1_flat.json',
+        'file1_yml_flat': './tests/fixtures/yaml/file1_flat.yml',
+        'file1_yaml_flat': './tests/fixtures/yaml/file1_flat.yaml',
+        'file2_json_flat': './tests/fixtures/json/file2_flat.json',
+        'file2_yml_flat': './tests/fixtures/yaml/file2_flat.yml',
+        'file2_yaml_flat': './tests/fixtures/yaml/file2_flat.yaml',
+
+        'file1_json_nested': './tests/fixtures/json/file1_nested.json',
+        'file1_yml_nested': './tests/fixtures/yaml/file1_nested.yml',
+        'file1_yaml_nested': './tests/fixtures/yaml/file1_nested.yaml',
+        'file2_json_nested': './tests/fixtures/json/file2_nested.json',
+        'file2_yml_nested': './tests/fixtures/yaml/file2_nested.yml',
+        'file2_yaml_nested': './tests/fixtures/yaml/file2_nested.yaml',
+
+        'stylish_flat': './tests/fixtures/result/stylish_flat.txt',
+        'stylish_nested': './tests/fixtures/result/stylish_nested.txt',
+
+        'plain_flat': './tests/fixtures/result/plain_flat.txt',
+        'plain_nested': './tests/fixtures/result/plain_nested.txt',
+    }
+    return links
 
 
-@pytest.fixture
-def yaml_plain():
-    link = {}
-    link['before'] = './tests/fixtures/file1_plain.yml'
-    link['after'] = './tests/fixtures/file2_plain.yml'
-    link['result'] = './tests/fixtures/result_plain.txt'
-    return link
-
-
-@pytest.fixture
-def json_nested():
-    link = {}
-    link['before'] = './tests/fixtures/file1_nested.json'
-    link['after'] = './tests/fixtures/file2_nested.json'
-    link['result'] = './tests/fixtures/result_nested.txt'
-    return link
-
-
-@pytest.fixture
-def yaml_nested():
-    link = {}
-    link['before'] = './tests/fixtures/file1_nested.yaml'
-    link['after'] = './tests/fixtures/file2_nested.yaml'
-    link['result'] = './tests/fixtures/result_nested.txt'
-    return link
-
-def test_json_plain(json_plain):
-    with open(json_plain['result']) as file:
-        expected = file.read()    
-    assert generate_diff(json_plain['before'], json_plain['after'], '') == expected
-
-
-def test_yaml_plain(yaml_plain):
-    with open(yaml_plain['result']) as file:
-        expected = file.read()    
-    assert generate_diff(yaml_plain['before'], yaml_plain['after'], '') == expected
-
-
-def test_json_nested(json_nested):
-    with open(json_nested['result']) as file:
+def test_stylish_flat(files):
+    with open(files['stylish_flat']) as file:
         expected = file.read()
-    assert generate_diff(json_nested['before'], json_nested['after'], '') == expected
+    assert generate_diff(files['file1_json_flat'], files['file2_json_flat'], 'stylish') == expected
+    assert generate_diff(files['file1_yml_flat'], files['file2_yml_flat'], 'stylish') == expected
+    assert generate_diff(files['file1_yaml_flat'], files['file2_yaml_flat'], 'stylish') == expected
 
 
-def test_yaml_nested(yaml_nested):
-    with open(yaml_nested['result']) as file:
+def test_stylish_nested(files):
+    with open(files['stylish_nested']) as file:
         expected = file.read()
-    assert generate_diff(yaml_nested['before'], yaml_nested['after'], '') == expected
+    assert generate_diff(files['file1_json_nested'], files['file2_json_nested'], 'stylish') == expected
+    assert generate_diff(files['file1_yml_nested'], files['file2_yml_nested'], 'stylish') == expected
+    assert generate_diff(files['file1_yaml_nested'], files['file2_yaml_nested'], 'stylish') == expected
+
+
+# def test_plain_flat(files):
+#     with open(files['plain_flat']) as file:
+#         expected = file.read()
+#     assert generate_diff(files['file1_json_flat'], files['file2_json_flat'], 'plain') == expected
+#     assert generate_diff(files['file1_yml_flat'], files['file2_yml_flat'], 'plain') == expected
+#     assert generate_diff(files['file1_yaml_flat'], files['file2_yaml_flat'], 'plain') == expected
+#
+#
+# def test_plain_nested(files):
+#     with open(files['plain_nested']) as file:
+#         expected = file.read()
+#     assert generate_diff(files['file1_json_nested'], files['file2_json_nested'], 'plain') == expected
+#     assert generate_diff(files['file1_yml_nested'], files['file2_yml_nested'], 'plain') == expected
+#     assert generate_diff(files['file1_yaml_nested'], files['file2_yaml_nested'], 'plain') == expected
+
