911a6854bb61343e31d611692a0376e260aa5742
Andrey Ivanov <veny.xd@gmail.com>
2022-11-23 10:56:41 +0100

    add type hints

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,8 +13,9 @@ def main() -> NoReturn:
     :return: NoReturn
     """
     args = parse_arguments()
-    print(generate_diff(args.first_file, args.second_file, args.format))
+    generate_diff(args.first_file, args.second_file, args.format)
+    # print(generate_diff(args.first_file, args.second_file, args.format))
 
 
-if __name__ == ('__main__'):
+if __name__ == '__main__':
     main()
