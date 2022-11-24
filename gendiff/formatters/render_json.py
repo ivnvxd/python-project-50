1c1259d709380a25f11613798c8f8eb667ce7f35
Andrey Ivanov <veny.xd@gmail.com>
2022-11-24 19:54:01 +0100

    fix cognitive complexity

--- gendiff/formatters/render_json.py
+++ gendiff/formatters/render_json.py
@@ -18,38 +18,45 @@ def make_json(diff: list) -> str:
     :return: String of difference visualization in "json" format.
     """
 
-    def iter_(data: Any) -> dict:
-
-        diff_dict = {}
-        data.sort(key=lambda node: node['key'])
-
-        for node in data:
-
-            if node['type'] in [REMOVED, UNCHANGED]:
-                diff_dict[node['key']] = {
-                    'value': node['old_value']
-                }
-
-            elif node['type'] == ADDED:
-                diff_dict[node['key']] = {
-                    'value': node['new_value']
-                }
-
-            elif node['type'] == UPDATED:
-                diff_dict[node['key']] = {
-                    'value': node['old_value'],
-                    'new value': node['new_value']
-                }
-
-            elif node['type'] == NESTED:
-                diff_dict[node['key']] = {
-                    'value': iter_(node['children'])
-                }
-
-            diff_dict[node['key']]['type'] = node['type']
-
-        return diff_dict
-
     result = json.dumps(iter_(diff), indent=4)
 
     return result
+
+
+def iter_(data: Any) -> dict:
+    """
+    Iterates through difference tree node and returns a difference dict.
+
+    :param data: Difference node.
+    :return: Difference dict.
+    """
+
+    diff_dict = {}
+    data.sort(key=lambda node: node['key'])
+
+    for node in data:
+
+        if node['type'] in [REMOVED, UNCHANGED]:
+            diff_dict[node['key']] = {
+                'value': node['old_value']
+            }
+
+        elif node['type'] == ADDED:
+            diff_dict[node['key']] = {
+                'value': node['new_value']
+            }
+
+        elif node['type'] == UPDATED:
+            diff_dict[node['key']] = {
+                'value': node['old_value'],
+                'new value': node['new_value']
+            }
+
+        elif node['type'] == NESTED:
+            diff_dict[node['key']] = {
+                'value': iter_(node['children'])
+            }
+
+        diff_dict[node['key']]['type'] = node['type']
+
+    return diff_dict
